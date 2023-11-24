c7c17d514c6f894f18ed0c8ee5b7067187359a0e
tot0p <thomas.lemaitre.78420@gmail.com>
2023-11-24 14:44:58 +0100

    update

--- WAKATIME/main.py
+++ WAKATIME/main.py
@@ -8,7 +8,7 @@ blc = ">-"
 configMermaid = [
     "### Pie chart of Time repartition\n"
     "```mermaid\n",	
-    "%%{init: {\"pie\": {\"textPosition\": 0.5}, \"themeVariables\": {\"pieOuterStrokeWidth\": \"5px\",\"primaryTextColor\" : \"#2F4F4F\"},\"theme\":\"base\"} }%%\n",
+    "%%{init: {\"pie\": {\"textPosition\": 0.5}, \"themeVariables\": {\"pieOuterStrokeWidth\": \"5px\",\"primaryTextColor\" : \"#778899\"},\"theme\":\"base\"} }%%\n",
     "pie\n",
 ]
 
